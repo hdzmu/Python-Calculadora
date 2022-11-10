7ac5bdeef9bfd8e06e07332963e70fc0e7abdb65
Mateo Hernandez Muñoz <mateo.hernandezm@udea.edu.co>
2022-11-10 14:25:55 -0500

    entrys

--- Calculadora/Interfaz.py
+++ Calculadora/Interfaz.py
@@ -27,7 +27,18 @@ miLabel=Label(miFrame, text="Calculadora")
 miLabel.place(x=350,y=10) #similar al pack() pero no cambia el frame
 miLabel.config(bg="gray")
 miLabel.config(font=("Comic Sans MS",18), fg="white")
-Label(miFrame, text="Cientifica", bg="black", fg="white", font=("Comic Sans MS",18)).place(x=1,y=10)
+#Label(miFrame, text="Cientifica", bg="black", fg="white", font=("Comic Sans MS",18)).place(x=1,y=10)
+
+#CUADROS DE TEXTO
+Label(miFrame, text="Ingrese numeros: ").grid(row=0,column=0, sticky="w") #(grid)alinear mediante filas y colomnnas - sticky-> alinear
+texto=Entry(miFrame)
+texto.grid(row=0,column=1)
+
+Label(miFrame, text="Ingrese 2: ").grid(row=1,column=0, sticky="w") #alinear mediante filas y colomnnas
+Entry(miFrame).grid(row=1,column=1)
+
+Label(miFrame, text="JJJJJJJJJAAAAAAAAAAJ").grid(row=2,column=0, sticky="w") #(grid)alinear mediante filas y colomnnas
+Entry(miFrame).grid(row=2,column=1)
 
 
 raiz.mainloop() #bucle infinito
