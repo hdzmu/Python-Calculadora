e0ac465eb47ce15ecbdc2b62ccc427459795580a
victormMcc2486 <victormcc2486@hotmail.com>
2022-11-19 13:15:40 -0500

    Derivadas
    Derivadas de polinomios, parciales, trigonométricas y otros

--- Calculadora/Funciones_Matematicas.py
+++ Calculadora/Funciones_Matematicas.py
@@ -5,4 +5,14 @@ print(type(numerocadena))
 print(np.sin(numerocadena*(np.pi/180)))'''
 
 #victor ingrese su funcion
+import sympy
+from sympy import *
+
+def derivarPolinomios(texto, sim,orden):
+    x = symbols(sim)
+    if orden == 0 or orden == None: orden = 1
+    #funcion = sympy.Poly(texto)
+    derivada = diff(texto, x, orden)
+
+    return (derivada)
 
