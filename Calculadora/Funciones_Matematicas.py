5621a45c81820b868d8e251ea3260b2887c742e0
victormMcc2486 <victormcc2486@hotmail.com>
2022-11-24 10:23:55 -0500

    Actualización Funciones Matematicas
    Correccion de algunos errores al derivar y una prueba piloto de integración indefinida
    NOTA: No está enlazado con la parte gráfica todavía

--- Calculadora/Funciones_Matematicas.py
+++ Calculadora/Funciones_Matematicas.py
@@ -5,11 +5,11 @@ print(type(numerocadena))
 print(np.sin(numerocadena*(np.pi/180)))'''
 
 #victor ingrese su funcion
-import sympy
+import sympy as smp
 from sympy import *
 
 def derivarPolinomios(texto, sim, orden):
-    x = symbols(sim)
+    x = smp.symbols(sim)
     if orden == 0 or orden == None: orden = 1
     #funcion = sympy.Poly(texto)
     derivada = diff(texto, x, orden)
@@ -17,4 +17,48 @@ def derivarPolinomios(texto, sim, orden):
     return (derivada)
 
 print(type(derivarPolinomios("exp(x**2)","x",1)))
+
+def solosympy(texto, sim):
+    x, C = smp.symbols(sim + ' C')
+    subtexto = ""
+    cantP1=0
+    cantP2=0
+    funcion = ""
+    new = texto
+    for i in range(len(texto)):
+        #PRIMERO SE SELECCIONA QUE FUNCION VA A HACER
+        if(texto[i-1] =="f" and texto[i] == "¡"):
+            funcion = "derivar"
+        elif(texto[i-1] =="S" and texto[i] == "¡"):
+            funcion = "integrar"
+
+
+        #LUEGO SE OPERA LO SEGÚN LA FUNCIÓN
+        elif(funcion == "derivar"):
+            if(texto[i]== "("):
+                cantP1 += 1
+            elif(texto[i]== ")"):
+                cantP2 += 1
+            subtexto += texto[i]
+            if(cantP2 == cantP1):
+                funcion = ""
+                reemplazar = "f¡"+subtexto
+                new = new.replace(reemplazar,str(diff(subtexto,x)))
+                subtexto = ""
+        elif(funcion == "integrar"):
+            if(texto[i]== "("):
+                cantP1 += 1
+            elif(texto[i]== ")"):
+                cantP2 += 1
+            subtexto += texto[i]
+            if(cantP2 == cantP1):
+                funcion = ""
+                reemplazar = "S¡"+subtexto
+                new = new.replace(reemplazar,str(integrate(subtexto,x)+C))
+                subtexto = ""
+    
+    return (smp.sympify(new))
+
+    
+print(solosympy("(x**2+x+3*x) + f¡(E**(x**2)-(3+8*x))+S¡(2*x)",'x'))
 
