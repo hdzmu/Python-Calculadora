8a81e1b5d975d483f509996c5d0fb713950fa2bf
victormMcc2486 <victormcc2486@hotmail.com>
2022-11-30 18:15:59 -0500

    borrador

---
+++ Calculadora/Código de conversipon para Calculadora.py
@@ -0,0 +1,210 @@
+#Caso mio derivar
+import sympy as smp
+from sympy import *
+
+def derivarPolinomios(texto, sim, orden):
+    x = symbols(sim)
+    if orden == 0 or orden == None: orden = 1
+    #funcion = sympy.Poly(texto)
+    derivada = diff(texto, x, orden)
+
+    return (derivada)
+
+def convertir(texto):
+    new = ""
+    salto = 0
+    operacion = ""
+    for i in texto:
+        if(salto==0):
+            if(i=='ₓ'):
+                new+='*'
+            elif(i=='·'):
+                new+='*'
+            elif(i=='π'):
+                new+="*np.pi"
+            elif(i=='²'):
+                new+="**2"
+            elif(i=='ˆ'):
+                new+="**"
+            elif(i=='÷'):
+                new+="/"
+            #elif(i=='e'):
+                #new+=operacionEspecial(funcion,'e')
+                #salto=len(operacionEspecial(funcion,'e'))
+            #elif(i=='√'):
+                #new+=operacionEspecial(funcion,'√')
+                #salto=len(operacionEspecial(funcion,'√'))
+            elif(i=='ƒ' or i== '՚'):
+                operacion = "ƒ"
+                continue
+            elif(i==","):
+                break
+            else:
+                new+=i
+
+def operarParentesis(texto):
+    cantP1 = 0
+    cantP2 = 0
+    posicionP1 = 0
+    posicionP2 = 0
+    operacion = ""
+    activar = True
+
+    #intento = 1
+    
+    while(activar):
+        cantP1 = 0
+        cantP2 = 0
+        operacion = ""
+        print("Texto: ",texto)
+        if (len(texto)==1 ): activar =False
+        for i in range(len(texto)):
+            if(texto[i] =="("):
+                cantP1 += 1
+                posicionP1 = i
+                #Aca identificar la función o la operación
+                operacion = identificarOperacion(texto,i)
+            elif(texto[i] ==")"):
+                cantP2 += 1
+                if(cantP2 == 1):
+                    posicionP2 = i
+        if(cantP1 != cantP2):
+            print("Error, cierre parentesis")
+            activar = False
+        elif (cantP1==0 and cantP2==0):
+            activar = False
+        else:
+            subtexto = ""
+            for j in range(posicionP1,posicionP2+1,1):
+                subtexto += texto[j]
+            print("subtexto: ", subtexto)
+            texto = texto.replace(subtexto, operar(subtexto, operacion))
+            
+            #Comprovar iteraciones
+            #if intento == 2:
+            #    activar = False
+            #intento +=1
+
+
+
+def OperarParentesis(texto, posicionP1, posicionP2):
+    #OBSOLETO YA SE MEJORÓ ARRIBA
+    subtexto = ""
+    for i in range(len(posicionP1),0,-1):
+        subtexto = ""
+        for j in range(posicionP1[i-1],posicionP2[i-1] + 1,1):
+            subtexto += texto[j]
+        print(subtexto)
+
+def identificarOperacion(texto, posicion):
+    operacion = ""  
+    if(texto[posicion-3]== "s" and texto[posicion-2]== "e" and texto[posicion-1]== "n"): operacion = "sen"
+    elif(texto[posicion-3]== "c" and texto[posicion-2]== "o" and texto[posicion-1]== "s"): operacion = "cos"
+    elif(texto[posicion-3]== "t" and texto[posicion-2]== "a" and texto[posicion-1]== "n"): operacion = "tan"
+    elif(texto[posicion-3]== "c" and texto[posicion-2]== "s" and texto[posicion-1]== "c"): operacion = "csc"
+    elif(texto[posicion-3]== "s" and texto[posicion-2]== "e" and texto[posicion-1]== "c"): operacion = "sec"
+    elif(texto[posicion-3]== "c" and texto[posicion-2]== "o" and texto[posicion-1]== "t"): operacion = "cot"
+    elif(texto[posicion-2]== "*" and texto[posicion-1]== "*"): operacion = "**"
+    elif(texto[posicion-1]== "^"): operacion = "**"
+    else:
+        operacion = texto[posicion-1]
+    return operacion
+
+def operar(subtexto,operacion):
+    #Hacer la operación que se desee
+    resolver = ""
+    for i in subtexto:
+        if (i=="x"):
+            resolver = smp.Poly(subtexto)
+            break
+        else:
+            #resolver = eval(subtexto)
+            continue
+    print("operación/funcion: ",operacion)
+    print(resolver)
+    return resolver
+
+#operarParentesis("1*2^(6*(x+sen(x+3+5)))")
+#contarParentesis("2+(6*(x+(x+3)))")
+
+def solosympy(texto, sim):
+    x, C = smp.symbols(sim + ' C')
+    subtexto = ""
+    arreglo = {}
+    cantP1=0
+    cantP2=0
+    anidado = False
+    funcion = ""
+    new = texto
+    for i in range(len(texto)):
+        #PRIMERO SE SELECCIONA QUE FUNCION VA A HACER (SE MODIFICA PARA INTEGRARLO LUEGO)
+        if(texto[i-1] =="f" and texto[i] == "¡"):
+            funcion = "derivar"
+            if(anidado):
+                subtexto += texto[i]
+            anidado = True
+        elif(texto[i-1] =="S" and texto[i] == "¡"):
+            funcion = "integrar"
+
+        #LUEGO SE OPERA LO SEGÚN LA FUNCIÓN
+        elif(funcion == "derivar"):
+            #print("El texto es: ",texto[i])
+            if(texto[i]== "("):
+                cantP1 += 1
+            elif(texto[i]== ")"):
+                cantP2 += 1
+            subtexto += texto[i]
+            print(subtexto)
+            if(cantP2 == cantP1):
+                #print(subtexto)
+                funcion = ""
+                anidado = False
+                #reemplazar = "f¡"+subtexto
+                #new = new.replace(reemplazar,str(diff(subtexto,x)))
+                subtexto = ""
+        elif(funcion == "integrar"):
+            if(texto[i]== "("):
+                cantP1 += 1
+            elif(texto[i]== ")"):
+                cantP2 += 1
+            subtexto += texto[i]
+            if(cantP2 == cantP1):
+                funcion = ""
+                #print(diff(subtexto,x))
+                #print(subtexto)
+                reemplazar = "S¡"+subtexto
+                #print(reemplazar)
+                new = new.replace(reemplazar,str(integrate(subtexto,x)+C))
+                subtexto = ""
+    
+    return (smp.sympify(new))
+
+    
+print(solosympy("f¡(x**2+f¡(x**2+f¡(5*x))+f¡(x**2))+ f¡(x)",'x'))
+
+def zzz(f):
+    pos=[]
+    inicio=1
+    fin=0
+    veces=1
+    for i in range(2,len(f)-1,1):
+        if f[i] =='¡':
+            fin=i
+            pos.append([inicio,fin])
+            inicio=fin
+            veces+=1
+    
+    if veces>1:
+        pos.append([inicio,len(f)-1])
+    print(pos)
+    print(len(pos))
+
+#zzz("f¡[sin(x)+f¡(cos(x))]+f¡(x)")
+#x='x'
+#funcion2='3*(x*x)'
+
+#funcion=str("x+x+smp.diff(x**2)")
+
+
+#print(eval(str("smp.diff(funcion)")))
+print(i)
